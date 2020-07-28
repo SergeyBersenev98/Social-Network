57227e1cd758aff483f27ec78029ffe19f86a827
SergeyBersenev98 <bersenevsergey12@mail.ru>
2020-07-28 22:31:23 +0300

    HOC

---
+++ src/HOC/AuthRedirect.js
@@ -0,0 +1,22 @@
+import {Redirect} from 'react-router-dom'
+import React from 'react';
+import {connect} from 'react-redux'
+
+let f1ForRedirect = (state) => ({
+  isAuth: state.AuthorizationUser.isAuth
+})
+
+export const  AuthRedirect = (Component) => {
+  class RedirectComponent extends React.Component {
+    render () {
+      if (!this.props.isAuth) return <Redirect to = '/Login' />
+        return <Component {...this.props} /> 
+    }
+  }
+  let ConnectedAuthRedirectComponent = connect(f1ForRedirect)(RedirectComponent)
+    return ConnectedAuthRedirectComponent
+}
+
+
+
+export default AuthRedirect;
