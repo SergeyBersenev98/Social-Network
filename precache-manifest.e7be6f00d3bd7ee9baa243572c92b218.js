d6ee879dbc2b02c3d48987c1fc6a7a87d4b3e592
SergeyBersenev98 <66181138+SergeyBersenev98@users.noreply.github.com>
2020-09-10 15:56:56 +0300

    Add files via upload

---
+++ precache-manifest.e7be6f00d3bd7ee9baa243572c92b218.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e5da30a0407b2cf083d5fc7339e9955",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf2583399619d253f7b2",
+    "url": "/static/css/main.8f2f481c.chunk.css"
+  },
+  {
+    "revision": "256754a264d6024ee4d0",
+    "url": "/static/js/2.9c8a211f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9c8a211f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf2583399619d253f7b2",
+    "url": "/static/js/main.6a5f080a.chunk.js"
+  },
+  {
+    "revision": "4fb080de24627b03831e",
+    "url": "/static/js/runtime-main.f16ea130.js"
+  },
+  {
+    "revision": "c8b7ff20d0c0a86dadc1fa5fa77f58dc",
+    "url": "/static/media/AlexBrush.c8b7ff20.ttf"
+  },
+  {
+    "revision": "fa099024ec60864b3a1fd7ee2ad44a9e",
+    "url": "/static/media/ava.fa099024.jpg"
+  },
+  {
+    "revision": "1000884647a3d65732a85e50e7b5029b",
+    "url": "/static/media/cityDay.10008846.png"
+  },
+  {
+    "revision": "5e2312aad7e019755cc89f5ae46f2e28",
+    "url": "/static/media/cityNight.5e2312aa.png"
+  },
+  {
+    "revision": "8f02038e5e82da3474322bd797a0355f",
+    "url": "/static/media/loading.8f02038e.gif"
+  },
+  {
+    "revision": "bba4c3ec736bae5ec7292df0aad6aac9",
+    "url": "/static/media/loadingTwilight.bba4c3ec.gif"
+  },
+  {
+    "revision": "c4616ee9194dc9f19006d5ecb3fff82a",
+    "url": "/static/media/noAva.c4616ee9.jpg"
+  },
+  {
+    "revision": "fddc07daa5f204fae7f3da5802f8168f",
+    "url": "/static/media/nophoto.fddc07da.PNG"
+  },
+  {
+    "revision": "115d67569a2a049a7460b763984b506f",
+    "url": "/static/media/nophotosmall.115d6756.PNG"
+  },
+  {
+    "revision": "e24851d830f0dfb38a7046f63f277368",
+    "url": "/static/media/unnamed.e24851d8.jpg"
+  }
+]);
