8755244864bacc586d141966b543446465382829
SergeyBersenev98 <66181138+SergeyBersenev98@users.noreply.github.com>
2020-09-10 18:22:55 +0300

    built files were added

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7ca5cf5897a199702e033d2379f1b329.js"
+  "/precache-manifest.ab107140dd9c570a53d864dcab4b86ce.js"
 );
 
 self.addEventListener('message', (event) => {
